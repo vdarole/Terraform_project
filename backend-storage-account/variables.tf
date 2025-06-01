ab9acc90111b6debacc80eb8eae8b865bc0ef360
anupam roy <royanup1990@gmail.com>
2025-06-02 00:08:23 +0530

    added the comments in variables.tf file

--- backend-storage-account/variables.tf
+++ backend-storage-account/variables.tf
@@ -1,13 +1,11 @@
-variable "rg_name" {
-  type        = string
-  description = "resource group name"
-}
+/*
+service principle credentials shouldnt be defined here 
+with sensitive as true if we are defining these sensitive variables
+in azure devops pipeline or any other cicd pipelines as environment
+variables else it wont work.
+that is the reason why i have commented out here..
 
-variable "region_location" {
-  type        = string
-  description = "resource group location"
-}
-
+*/
 # variable "subscription_id" {
 #   type      = string
 #   sensitive = true
@@ -27,6 +25,16 @@ variable "region_location" {
 #   type      = string
 #   sensitive = true
 # }
+
+variable "rg_name" {
+  type        = string
+  description = "resource group name"
+}
+
+variable "region_location" {
+  type        = string
+  description = "resource group location"
+}
 
 variable "storage_account_name" {
   type = string
