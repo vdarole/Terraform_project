5e7a671b68c1a6b83f9981dbb0297b77776b439a
anupam roy <royanup1990@gmail.com>
2025-06-01 22:15:34 +0530

    updated the vars

--- backend-storage-account/variables.tf
+++ backend-storage-account/variables.tf
@@ -8,25 +8,25 @@ variable "region_location" {
   description = "resource group location"
 }
 
-variable "subscription_id" {
-  type      = string
-  sensitive = true
-}
+# variable "subscription_id" {
+#   type      = string
+#   sensitive = true
+# }
 
-variable "client_id" {
-  type      = string
-  sensitive = true
-}
+# variable "client_id" {
+#   type      = string
+#   sensitive = true
+# }
 
-variable "client_secret" {
-  type      = string
-  sensitive = true
-}
+# variable "client_secret" {
+#   type      = string
+#   sensitive = true
+# }
 
-variable "tenant_id" {
-  type      = string
-  sensitive = true
-}
+# variable "tenant_id" {
+#   type      = string
+#   sensitive = true
+# }
 
 variable "storage_account_name" {
   type = string
