b26d334a047aa61aa76948d85c399219868425ec
anupam roy <royanup1990@gmail.com>
2025-06-02 18:10:09 +0530

    commented out

--- variables.tf
+++ variables.tf
@@ -8,22 +8,22 @@ variable "location" {
   description = "resource group location"
 }
 
-variable "subscription_id" {
-  type = string
-  sensitive = true
-}
+# variable "subscription_id" {
+#   type = string
+#   sensitive = true
+# }
 
-variable "client_id" {
-  type = string
-  sensitive = true
-}
+# variable "client_id" {
+#   type = string
+#   sensitive = true
+# }
 
-variable "client_secret" {
-  type = string
-  sensitive = true
-}
+# variable "client_secret" {
+#   type = string
+#   sensitive = true
+# }
 
-variable "tenant_id" {
-  type = string
-  sensitive = true
-}
+# variable "tenant_id" {
+#   type = string
+#   sensitive = true
+# }
