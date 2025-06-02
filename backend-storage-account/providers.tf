1e3469360dc0212eaec7234d5c23aebf25ecdb67
anupam roy <royanup1990@gmail.com>
2025-06-02 17:35:39 +0530

    removed backend resource code from here

--- backend-storage-account/providers.tf
+++
@@ -1,7 +0,0 @@
-provider "azurerm" {
-  features {}
-  # subscription_id = var.subscription_id
-  # client_id       = var.client_id     ## Registered App's ID 
-  # client_secret   = var.client_secret ## client secret value
-  # tenant_id       = var.tenant_id     ## Registered App's tenant ID 
-}
--- backend-storage-account/terraform.tfvars
+++
@@ -1,5 +0,0 @@
-rg_name                = "rg-terraform-backend"
-region_location        = "EAST US"
-storage_account_name   = "roytfstate123"
-storage_container_name = "tfstateenvs"
-  
