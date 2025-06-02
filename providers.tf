9625285aa1e9aad32d8c4847e7a7d0f629151ed1
anupam roy <royanup1990@gmail.com>
2025-06-02 17:56:39 +0530

    commented the SP creds vars

--- providers.tf
+++ providers.tf
@@ -1,7 +1,7 @@
 provider "azurerm" {
   features {}
-  subscription_id = var.subscription_id
-  client_id       = var.client_id  ## Registered App's ID 
-  client_secret   = var.client_secret ## client secret value
-  tenant_id       = var.tenant_id ## Registered App's tenant ID 
+  # subscription_id = var.subscription_id
+  # client_id       = var.client_id  ## Registered App's ID 
+  # client_secret   = var.client_secret ## client secret value
+  # tenant_id       = var.tenant_id ## Registered App's tenant ID 
 }
